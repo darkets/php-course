e7ca3a5b1aa1e358f2a066d301293e7559900d7a
Roberts Bauers <rrbauerss@gmail.com>
2023-09-13 00:02:43 +0300

    define min & max range

--- 02-if-else-elseif/02.php
+++ 02-if-else-elseif/02.php
@@ -3,11 +3,13 @@
 // Given variable (int) 50, determine if it's in the range of 1 and 100.
 
 $number = 50;
+$rangeMin = 1;
+$rangeMax = 100;
 
-if ($number >= 1 && $number <= 100) {
-    echo "$number is in the range of 1 and 100";
+if ($number >= $rangeMin && $number <= $rangeMax) {
+    echo "$number is in the range of $rangeMin and $rangeMax";
 } else {
-    echo "$number is not in the range of 1 and 100";
+    echo "$number is not in the range of $rangeMin and $rangeMax";
 }
 
 // Output
