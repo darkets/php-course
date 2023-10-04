59faaa3c7440808519b89e73cacefa035f767179
Roberts Bauers <rrbauerss@gmail.com>
2023-10-04 15:47:55 +0300

    implement main function

--- php-basics/classes-and-objects/01/main.php
+++ php-basics/classes-and-objects/01/main.php
@@ -2,14 +2,23 @@
 
 require_once 'php-basics/classes-and-objects/01/Product.php';
 
+class Main
+{
+    public static function main()
+    {
+        $mouse = new Product('Logitech mouse', 70.00, 14);
+        $phone = new Product('iPhone 5s', 999.99, 3);
+        $projector = new Product('Epson EB-U05', 440.46, 1);
 
-$mouse = new Product('Logitech Mouse', 70.00, 14);
-$phone = new Product('iPhone 5s', 999.99, 3);
-$projector = new Product('Epson EB-U05', 440.46, 1);
+        $mouse->printProduct();
+        $phone->printProduct();
+        $projector->printProduct();
 
-$mouse->printProduct();
-$phone->printProduct();
-$projector->printProduct();
+        $mouse->setPrice(75.00);
+        $mouse->setAmount(20);
 
-$mouse->setPrice(64.99);
-$projector->setAmount(3);
+        $mouse->printProduct();
+    }
+}
+
+Main::main();
